8d6627dcf572de9cc57db767d9d6bf3438a854dc
moeabdol <admin.r99@gmail.com>
2017-06-23 16:39:17 +0300

    Getting the tests running again

---
+++ spec/spec_helper_lite.rb
@@ -0,0 +1,9 @@
+def stub_module(full_name)
+  full_name.split(/::/).inject(Object) do |context, name|
+    begin
+      context.const_get(name)
+    rescue
+      context.const_set(name, Module.new)
+    end
+  end
+end
